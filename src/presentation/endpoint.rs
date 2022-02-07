46438f64a2a5717151226dd18a7338f7702f5899
yotaro-shimose <yosemat.beta@gmail.com>
2022-02-07 16:03:29 +0900

    presentationの実装

--- src/presentation/endpoint.rs
+++ src/presentation/endpoint.rs
@@ -1,8 +1,10 @@
+mod fetch_calendar;
 mod fetch_task;
 mod fetch_task_list;
 mod fetch_user_config;
 mod id_header;
 mod login;
+pub use fetch_calendar::fetch_calendar;
 pub use fetch_task::fetch_task;
 pub use fetch_task_list::fetch_task_list;
 pub use fetch_user_config::fetch_user_config;
