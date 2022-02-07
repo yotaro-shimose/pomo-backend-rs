de8d00c6b514f6bc17176f790ae64f936bcd8803
yotaro-shimose <yosemat.beta@gmail.com>
2022-02-07 19:25:38 +0900

    update_user_config機能の完成

---
+++ src/usecase/update_user_config_usecase.rs
@@ -0,0 +1,18 @@
+use crate::domain::{
+    model::value::{UserConfig, UserId},
+    repository::DBRepository,
+};
+use actix_web::{error, Result};
+
+pub async fn update_user_config_usecase(
+    id: &UserId,
+    config: UserConfig,
+    db_repository: &impl DBRepository,
+) -> Result<()> {
+    let mut user = db_repository
+        .fetch_user(id)?
+        .ok_or_else(|| error::ErrorNotFound(format!("No User Matched Id {}", id)))?;
+    user.update_config(config);
+    db_repository.save_user(&user)?;
+    Ok(())
+}
