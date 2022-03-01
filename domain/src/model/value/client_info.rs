32fbf7bae52abafd0eb85405f7a24c1bf49de3f6
yotaro-shimose <yosemat.beta@gmail.com>
2022-03-01 13:42:29 +0900

    環境変数からもgoogle client infoを生成できるように

--- domain/src/model/value/client_info.rs
+++ domain/src/model/value/client_info.rs
@@ -1,7 +1,7 @@
-use actix_web::Result;
+use actix_web::{error, Result};
+use envy;
 use serde::Deserialize;
 use std::fs;
-
 const CLIENT_SECRET_PATH: &str = "./google_auth/client_secret.json";
 
 #[derive(Debug, Deserialize, Clone)]
@@ -18,7 +18,19 @@ pub struct ClientInfo {
 }
 
 impl ClientInfo {
-    pub fn load() -> Result<ClientInfo> {
+    pub fn load() -> Result<Self> {
+        if let Ok(info) = ClientInfo::load_from_env() {
+            Ok(info)
+        } else {
+            ClientInfo::load_from_file()
+        }
+    }
+
+    fn load_from_env() -> Result<Self> {
+        envy::from_env().map_err(error::ErrorInternalServerError)
+    }
+
+    fn load_from_file() -> Result<Self> {
         let file = fs::File::open(CLIENT_SECRET_PATH)?;
         let json: ClientSecretJson = serde_json::from_reader(file)?;
         Ok(json.web)
